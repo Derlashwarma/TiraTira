1c4d03b17b8e5ece5f5237fe2eeed9cf4d39250f
Subatomi <laderatheo@gmail.com>
2024-05-13 15:22:06 +0800

    22

---
+++ src/main/java/com/example/game/Entity/EnemyT1Bomber.java
@@ -0,0 +1,20 @@
+package com.example.game.Entity;
+
+import javafx.application.Platform;
+import javafx.scene.paint.Color;
+
+public class EnemyT1Bomber extends Enemy{
+    public EnemyT1Bomber(long speed, double size, double currentX, double currentY, Color color, String name) {
+        super(80, 20, 300 ,currentX, currentY, color, name);
+    }
+
+    @Override
+    public void move() {
+        Platform.runLater(() -> {
+            setLayoutX(getCurrentX()); // Update the layout X position
+            setLayoutY(getCurrentY()); // Update the layout Y position
+        });
+        setCurrentY(getCurrentY() + 1); // Increment Y position
+    }
+
+}
